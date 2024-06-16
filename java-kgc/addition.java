40fe896d97580f530600368fe5d83a6a9ecc5573
Shree Mangalam <sshreemangalam@gmail.com>
2024-06-16 22:20:41 +0530

    added sum

---
+++ java-kgc/addition.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class addition {
+  public static void main(String[] args) {
+    System.out.println("Addition of two numbers: ");
+    try(Scanner input = new Scanner(System.in)) {
+      System.out.print("Enter the first number: ");
+      int num1 = input.nextInt(); 
+      System.out.print("Enter the second number: ");
+      int num2 = input.nextInt(); 
+      int num3 = num1 + num2; 
+      System.out.println("The result of addition is: " + num3);
+    }
+  }
+}
