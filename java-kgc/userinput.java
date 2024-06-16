0c705d20a34e36d96d8affd53055b41f1e869995
Shree Mangalam <sshreemangalam@gmail.com>
2024-06-16 22:19:55 +0530

    added user-input

--- java-kgc/userinput.java
+++ java-kgc/userinput.java
@@ -1,12 +1,24 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class userinput {
-//   public static void main(String[] args) {
-//     System.out.println("userinput ");
+public class userinput {
+  public static void main(String[] args) {
+    System.out.println("userinput ");
+    try (Scanner input = new Scanner(System.in)) {
+      System.out.print("Enter your name: ");
+      String name = input.nextLine(); 
+      System.out.println("Good Morning "+ name);
+
+      System.out.print(name + " , tell me your age ");
+      int age = input.nextInt(); 
+      System.out.println("your age is: " + age);
+    }
     
-    
-//   }
-// }
+  }
+}
 
-
-
+/*
+ * nextInt()
+ * nextDouble()
+ * nextFloat()
+ * nextLong()
+ */
